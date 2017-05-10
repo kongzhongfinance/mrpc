df8ebe0e02bf29c31c94d5f88a3cdbd8d8f35400
biezhi <biezhi.me@gmail.com>
2017-05-10 18:04:52 +0800

    🐿 add nexus deploy config

---
+++ package.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cd mrpc-common
+mvn clean deploy -Durl=http://10.230.200.15:8081/nexus/content/repositories/releases/ -DrepositoryId=releases -P releases
+cd ..
+
+cd mrpc-core
+mvn clean deploy -Durl=http://10.230.200.15:8081/nexus/content/repositories/releases/ -DrepositoryId=releases -P releases
+cd ..
+
+cd mrpc-serialize/mrpc-serialize-kryo
+mvn clean deploy -Durl=http://10.230.200.15:8081/nexus/content/repositories/releases/ -DrepositoryId=releases -P releases
+cd ../../
+
+cd mrpc-registry-zk
+mvn clean deploy -Durl=http://10.230.200.15:8081/nexus/content/repositories/releases/ -DrepositoryId=releases -P releases
+cd ..
