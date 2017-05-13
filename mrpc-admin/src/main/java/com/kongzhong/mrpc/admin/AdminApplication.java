6087b9dd55f0da5d8463914aeb6880fbb03e67a7
biezhi <biezhi.me@gmail.com>
2017-05-14 01:50:25 +0800

    🐿 add mrpc admin project

---
+++ mrpc-admin/src/main/java/com/kongzhong/mrpc/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.kongzhong.mrpc.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author biezhi
+ *         2017/5/14
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
