a31825d4d9f92f24c8af792321e7534f7c0a881d
biezhi <biezhi.me@gmail.com>
2017-05-15 12:36:55 +0800

    🐿 delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-mvn clean install -N
-
-mvn clean install -pl mrpc-common -am -Dmaven.test.skip=true -U
-mvn clean install -pl mrpc-core -am -Dmaven.test.skip=true -U
-mvn clean install -pl mrpc-registry-zk -am -Dmaven.test.skip=true -U
-mvn clean install -pl mrpc-serialize/mrpc-serialize-kryo -am -Dmaven.test.skip=true -U
