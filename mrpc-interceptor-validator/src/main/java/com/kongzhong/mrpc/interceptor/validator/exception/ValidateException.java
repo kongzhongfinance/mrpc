32fa1ea787940b4578e8dcb795e5d849b9ca3aab
IFT8 <IFT8@qq.com>
2017-06-28 16:17:57 +0800

    ValidateException change to RuntimeException

--- mrpc-interceptor-validator/src/main/java/com/kongzhong/mrpc/interceptor/validator/exception/ValidateException.java
+++ mrpc-interceptor-validator/src/main/java/com/kongzhong/mrpc/interceptor/validator/exception/ValidateException.java
@@ -8,7 +8,7 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class ValidateException extends Exception {
+public class ValidateException extends RuntimeException {
     private static final long serialVersionUID = -5770807215499382620L;
 
     private final String errMsg;
