98b7480641a44811df72367230d04b103d5af8eb
IFT8 <IFT8@qq.com>
2017-09-15 15:14:18 +0800

    MDC remove

--- mrpc-core/src/main/java/com/kongzhong/mrpc/transport/http/HttpServerHandler.java
+++ mrpc-core/src/main/java/com/kongzhong/mrpc/transport/http/HttpServerHandler.java
@@ -172,6 +172,7 @@ public class HttpServerHandler extends SimpleServerHandler<FullHttpRequest> {
 
             HttpResponseInvoker responseCallback = new HttpResponseInvoker(rpcRequest, httpResponse, serviceBeanMap);
             SimpleRpcServer.submit(responseCallback, ctx);
+            MDC.remove(TraceConstants.TRACE_ID);
         }
     }
 
