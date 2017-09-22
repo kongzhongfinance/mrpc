553d8cde7fd81fb94b8a148cce7d4717d56ad5ce
biezhi <biezhi.me@gmail.com>
2017-09-22 14:12:53 +0800

    🥀 fix timeout exception not throw

--- mrpc-common/src/main/java/com/kongzhong/mrpc/exception/TimeoutException.java
+++ mrpc-common/src/main/java/com/kongzhong/mrpc/exception/TimeoutException.java
@@ -1,15 +1,12 @@
 package com.kongzhong.mrpc.exception;
 
-import lombok.NoArgsConstructor;
-
 /**
  * Rpc调用超时异常
  *
  * @author biezhi
  *         2017/4/20
  */
-@NoArgsConstructor
-public class TimeoutException extends Exception {
+public class TimeoutException extends RuntimeException {
 
     public TimeoutException(String message) {
         super(message);
