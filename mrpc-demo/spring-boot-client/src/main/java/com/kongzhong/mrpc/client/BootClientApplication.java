afa297937979548ca0bd69e71a79afddc7db0560
biezhi <biezhi.me@gmail.com>
2017-11-28 23:59:50 +0800

    ✨ add trace filter exclusions

--- mrpc-demo/spring-boot-client/src/main/java/com/kongzhong/mrpc/client/BootClientApplication.java
+++ mrpc-demo/spring-boot-client/src/main/java/com/kongzhong/mrpc/client/BootClientApplication.java
@@ -2,17 +2,15 @@ package com.kongzhong.mrpc.client;
 
 import com.kongzhong.mrpc.demo.service.UserService;
 import com.kongzhong.mrpc.trace.interceptor.TraceClientInterceptor;
-import com.kongzhong.mrpc.trace.interceptor.TraceFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Collections;
+import javax.servlet.DispatcherType;
 
 /**
  * @author biezhi
@@ -29,17 +27,15 @@ public class BootClientApplication {
 
     @Bean
     @ConditionalOnClass(TraceClientInterceptor.class)
-    public FilterRegistrationBean traceFilter(@Autowired TraceClientInterceptor traceClientInterceptor) {
-
-        TraceFilter traceFilter = new TraceFilter(traceClientInterceptor.getTraceClientAutoConfigure(), traceClientInterceptor.getAgent());
-
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
-        servletRegistrationBean.setName("TraceFilter");
-
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(traceFilter, servletRegistrationBean);
-        filterRegistrationBean.setName("TraceFilter");
-        filterRegistrationBean.setUrlPatterns(Collections.singletonList("/*"));
-        return filterRegistrationBean;
+    public FilterRegistrationBean filterRegistrationBean(@Autowired TraceClientInterceptor traceClientInterceptor) {
+        com.kongzhong.mrpc.trace.interceptor.TraceFilter traceFilter = new com.kongzhong.mrpc.trace.interceptor.TraceFilter(traceClientInterceptor.getTraceClientAutoConfigure(), traceClientInterceptor.getAgent());
+        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+        filterRegistration.setFilter(traceFilter);
+        filterRegistration.addUrlPatterns("/*");
+        filterRegistration.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico");
+        // filterRegistration.setAsyncSupported(true);
+        filterRegistration.setDispatcherTypes(DispatcherType.REQUEST);
+        return filterRegistration;
     }
 
     public static void main(String[] args) {
