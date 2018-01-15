002db0c604df83fcdd6a31ece0ab4ec54193b5af
biezhi <biezhi.me@gmail.com>
2018-01-15 17:18:41 +0800

    :arrow_up: release 0.1.2-RELEASE

--- mrpc-common/src/main/java/com/kongzhong/mrpc/Const.java
+++ mrpc-common/src/main/java/com/kongzhong/mrpc/Const.java
@@ -8,7 +8,7 @@ package com.kongzhong.mrpc;
  */
 public interface Const {
 
-    String VERSION = "0.1.2-SNAPSHOT";
+    String VERSION = "0.1.2-RELEASE";
 
     String BANNER = "\n" +
             "\t\t    __  _______  ____  ______\n" +
